4e585f25ee4befec01ae6ea7e6dfdb3a06b6d2ad
Kazuhiko Sakaguchi <pi8027@gmail.com>
2012-01-10 01:40:06 +0900

    refactoring

--- coq/Lambda/Lambda.v
+++ coq/Lambda/Lambda.v
@@ -25,14 +25,14 @@ Fixpoint shift (d c : nat) (t : term) : term :=
     | abs t1 => abs (shift d (S c) t1)
   end.
 
+Functional Scheme shift_ind := Induction for shift Sort Prop.
+
 Definition shifted2 (d c : nat) (t : term) : Prop :=
   exists t' : term, shift d c t' = t.
 
 Lemma shifted1_shift : forall d c t, shifted1 d c (shift d c t).
   intros.
-  revert d c.
-  induction t ; intros ; simpl.
-  case (le_dec c n) ; intros.
+  functional induction (shift d c t).
   apply svar2 ; omega.
   apply svar1 ; omega.
   apply sapp ; auto.
@@ -42,32 +42,20 @@ Qed.
 Theorem shifted1_is_shifted2 : forall d c t, shifted1 d c t -> shifted2 d c t.
   intros.
   induction H.
-  exists (var n).
-  simpl.
-  case (le_dec c n).
-  intros.
-  apply False_ind.
-  omega.
+  exists (var n) ; simpl.
+  case (le_dec c n) ; intros.
+  apply False_ind ; omega.
   auto.
-  exists (var (n - d)).
-  simpl.
+  exists (var (n - d)) ; simpl.
   case (le_dec c (n - d)) ; intros.
-  f_equal.
-  omega.
-  apply False_ind.
-  omega.
-  elim IHshifted1_1.
-  elim IHshifted1_2.
-  intros.
-  exists (app x0 x).
-  simpl.
+  f_equal ; omega.
+  apply False_ind ; omega.
+  elim IHshifted1_1 ; elim IHshifted1_2 ; intros.
+  exists (app x0 x) ; simpl.
   f_equal ; auto.
-  elim IHshifted1.
-  intros.
-  exists (abs x).
-  simpl.
-  f_equal.
-  auto.
+  elim IHshifted1 ; intros.
+  exists (abs x) ; simpl.
+  f_equal ; auto.
 Qed.
 
 Theorem shifted2_is_shifted1 : forall d c t, shifted2 d c t -> shifted1 d c t.
@@ -76,8 +64,7 @@ Theorem shifted2_is_shifted1 : forall d c t, shifted2 d c t -> shifted1 d c t.
   rewrite <- H.
   exact (shifted1_shift d c x).
 Qed.
-  
 
-
-
-
+(*
+Definition unshift (d c : nat) (t : term) (p : shifted2 d c t) : term :=
+*)
