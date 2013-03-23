d6e4e295a9fff35341517f9781a2c673016de4c6
Kazuhiko Sakaguchi <pi8027@gmail.com>
2013-03-23 12:50:39 +0900

    cont

--- coq/LC/LN.v
+++ coq/LC/LN.v
@@ -6,16 +6,24 @@ Require Import
 
 Set Implicit Arguments.
 
+Notation nopts n T := (iter n option T).
+
 Inductive term (V : Type) : Type :=
   var of V | app of term V & term V | abs of term (option V).
 
-Notation term' n V := (term (iter n option V)).
+Notation term' n V := (term (nopts n V)).
 
 Fixpoint tmap (V1 V2 : Type) (f : V1 -> V2) (t : term V1) : term V2 :=
   match t with
     | var a => var (f a)
     | app tl tr => app (tmap f tl) (tmap f tr)
     | abs t => abs (tmap (Option.map f) t)
+  end.
+
+Fixpoint somenth (T : Type) (n : nat) (a : T) : nopts n T :=
+  match n with
+    | 0 => a
+    | n.+1 => Some (somenth n a)
   end.
 
 Fixpoint substitute
@@ -27,13 +35,15 @@ Fixpoint substitute
     | abs t2' => abs (@substitute _ n.+1 (@tmap _ _ (@Some _) t1) t2')
   end.
 
+Arguments substitute {V} n t1 t2.
+
 Reserved Notation "t ->1b t'" (at level 70, no associativity).
 Reserved Notation "t ->bp t'" (at level 70, no associativity).
 
 Inductive betared1 : forall V, relation (term V) :=
   | betared1beta :
     forall V (t1 : term (option V)) (t2 : term V),
-    app (abs t1) t2 ->1b @substitute V 0 t2 t1
+    app (abs t1) t2 ->1b substitute 0 t2 t1
   | betared1appl :
     forall V (t1 t1' t2 : term V), t1 ->1b t1' -> app t1 t2 ->1b app t1' t2
   | betared1appr :
@@ -52,5 +62,5 @@ Inductive parred : forall V, relation (term V) :=
     forall V (t t' : term (option V)), t ->bp t' -> abs t ->bp abs t'
   | parredbeta :
     forall V (t1 t1' : term (option V)) (t2 t2' : term V),
-    t1 ->bp t1' -> t2 ->bp t2' -> app (abs t1) t2 ->bp @substitute V 0 t2' t1'
+    t1 ->bp t1' -> t2 ->bp t2' -> app (abs t1) t2 ->bp substitute 0 t2' t1'
   where "t ->bp t'" := (parred t t').
