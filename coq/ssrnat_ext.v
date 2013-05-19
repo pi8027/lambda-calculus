c0f4245de917af5ee7047b53161486097e60699d
Kazuhiko Sakaguchi <pi8027@gmail.com>
2013-05-19 23:41:57 +0900

    rewrite the ssromega tactic

--- coq/ssrnat_ext.v
+++ coq/ssrnat_ext.v
@@ -5,20 +5,13 @@ Require Import
 Ltac arith_hypo_ssrnat2coqnat :=
   match goal with
     | H : context [andb _ _] |- _ => let H0 := fresh in case/andP: H => H H0
-    | H : context [orb _ _] |- _ => case/orP: H => H
-    | H : context [?L <= ?R] |- _ => move/leP: H => H
-    | H : context [?L == ?R] |- _ => move/eqP: H => H
-    | H : context [addn ?L ?R] |- _ => rewrite -plusE in H
-    | H : context [subn ?L ?R] |- _ => rewrite -minusE in H
-    | H : context [muln ?L ?R] |- _ => rewrite -multE in H
-    | H : context [addn_rec ?L ?R] |- _ => rewrite /addn_rec in H
-    | H : context [subn_rec ?L ?R] |- _ => rewrite /subn_rec in H
-    | H : context [muln_rec ?L ?R] |- _ => rewrite /muln_rec in H
+    | H : context [orb _ _] |- _ => case/orP in H
+    | H : context [?L <= ?R] |- _ => move/leP in H
+    | H : context [?L == ?R] |- _ => move/eqP in H
   end.
 
 Ltac arith_goal_ssrnat2coqnat :=
-  rewrite ?NatTrec.trecE -?plusE -?minusE -?multE;
-  repeat match goal with
+  match goal with
     | |- is_true (andb _ _) => apply/andP; split
     | |- is_true (orb _ _) => apply/orP
     | |- is_true (_ <= _) => apply/leP
@@ -26,6 +19,7 @@ Ltac arith_goal_ssrnat2coqnat :=
   end.
 
 Ltac ssromega :=
-  repeat arith_hypo_ssrnat2coqnat ;
-  arith_goal_ssrnat2coqnat ;
-  (omega || apply False_ind; omega).
+  do ?unfold addn, subn, muln, addn_rec, subn_rec, muln_rec in *;
+  do ?arith_hypo_ssrnat2coqnat ;
+  do ?arith_goal_ssrnat2coqnat ;
+  omega.
