876a8a0c05124be70099d7ce58273f4941eb4eb7
Kazuhiko Sakaguchi <pi8027@gmail.com>
2013-06-07 07:16:17 +0900

    fix bug of ssromega

--- coq/ssrnat_ext.v
+++ coq/ssrnat_ext.v
@@ -7,9 +7,9 @@ Tactic Notation "find_minneq_hyp" constr(n) constr(m) :=
     | H : is_true (n <= m) |- _ => move/minn_idPl: (H)
     | H : is_true (m <= n) |- _ => move/minn_idPr: (H)
     | |- _ =>
-      let H0 := fresh "H" in
-      case/orP: (leq_total n m) => H0;
-      [move/minn_idPl: (H0) | move/minn_idPr: (H0)]
+      let H := fresh "H" in
+      case/orP: (leq_total n m) => H;
+      [move/minn_idPl: (H) | move/minn_idPr: (H)]
   end.
 
 Tactic Notation "find_maxneq_hyp" constr(n) constr(m) :=
@@ -17,15 +17,18 @@ Tactic Notation "find_maxneq_hyp" constr(n) constr(m) :=
     | H : is_true (n <= m) |- _ => move/maxn_idPr: (H)
     | H : is_true (m <= n) |- _ => move/maxn_idPl: (H)
     | |- _ =>
-      let H0 := fresh "H" in
-      case/orP: (leq_total n m) => H0;
-      [move/maxn_idPr: (H0) | move/maxn_idPl: (H0)]
+      let H := fresh "H" in
+      case/orP: (leq_total n m) => H;
+      [move/maxn_idPr: (H) | move/maxn_idPl: (H)]
   end.
 
 Ltac replace_minn_maxn :=
+  let H' := fresh "H" in
   match goal with
-    | H : context [minn ?n ?m] |- _ => move: H; find_minneq_hyp n m => -> H
-    | H : context [maxn ?n ?m] |- _ => move: H; find_maxneq_hyp n m => -> H
+    | H : context [minn ?n ?m] |- _ =>
+      find_minneq_hyp n m => H'; rewrite H' {H'} in H
+    | H : context [maxn ?n ?m] |- _ =>
+      find_maxneq_hyp n m => H'; rewrite H' {H'} in H
     | |- context [minn ?n ?m] => find_minneq_hyp n m => ->
     | |- context [maxn ?n ?m] => find_maxneq_hyp n m => ->
   end.
@@ -47,9 +50,9 @@ Ltac arith_goal_ssrnat2coqnat :=
   end.
 
 Ltac ssromega :=
-  do ?unfold addn, subn, muln, addn_rec, subn_rec, muln_rec in *;
   repeat (let x := fresh "x" in move => x);
   do ?replace_minn_maxn;
+  do ?unfold addn, subn, muln, addn_rec, subn_rec, muln_rec in *;
   do ?arith_hypo_ssrnat2coqnat;
   do ?arith_goal_ssrnat2coqnat;
   omega.
@@ -64,3 +67,5 @@ Goal forall m n, maxn (minn m n) m = m. ssromega; fail. Abort.
 Goal forall m n, maxn n (minn m n) = n. ssromega; fail. Abort.
 Goal forall m n, minn m n = m <-> m <= n. split; ssromega; fail. Abort.
 Goal forall m n, maxn m n = n <-> m <= n. split; ssromega; fail. Abort.
+Goal forall m n, minn m n + (m - n) = m. ssromega; fail. Abort.
+Goal forall m n, maxn m n - (n - m) = m. ssromega; fail. Abort.
